b76b3f26aa6007fc5ce40dc97b44e5219f9c1565
Caitlin Powers <cpowers@6river.com>
2020-02-08 10:34:21 -0500

    feat: Play around with netlify

--- src/urls.ts
+++ src/urls.ts
@@ -1,11 +1,10 @@
-//const baseUrl: string = 'https://server-dot-actions-server-260415.appspot.com';
-const baseUrl: string = 'http://localhost:8000';
-//const baseUrl: string = 'http://159.89.54.69:8000';
-export const ACTION_CARD_URL=`${baseUrl}/actioncards/`;
-export const LATEST_ACTION_CARD_URL=`${baseUrl}/actioncards/latest/`
-export const ACTION_URL=`${baseUrl}/actions/`;
-export const SURVEY_RESPONSE_URL=`${baseUrl}/surveyresponses/`;
-export const USER_SURVEY_RESPONSE_URL=`${baseUrl}/surveyresponses/user/`;
-export const SIGN_IN_URL=`${baseUrl}/token/`;
-export const SIGN_OUT_URL=`${baseUrl}/rest-auth/louout/`;
-export const CURRENT_USER_URL=`${baseUrl}/user/`;
+const baseUrl: string = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:8000';
+
+export const ACTION_CARD_URL = `${baseUrl}/actioncards/`;
+export const LATEST_ACTION_CARD_URL = `${baseUrl}/actioncards/latest/`
+export const ACTION_URL = `${baseUrl}/actions/`;
+export const SURVEY_RESPONSE_URL = `${baseUrl}/surveyresponses/`;
+export const USER_SURVEY_RESPONSE_URL = `${baseUrl}/surveyresponses/user/`;
+export const SIGN_IN_URL = `${baseUrl}/token/`;
+export const SIGN_OUT_URL = `${baseUrl}/rest-auth/louout/`;
+export const CURRENT_USER_URL = `${baseUrl}/user/`;
