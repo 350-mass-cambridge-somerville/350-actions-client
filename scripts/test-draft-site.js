9be11cf72ca12f992a77c412e7b1404500eeb61a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-09 14:06:10 -0500

    run Cypress against draft deploy

---
+++ scripts/test-draft-site.js
@@ -0,0 +1,22 @@
+// after Netlify deploys preview / draft site
+// we save deploy results in `deploy.json` file (see Netlify GH workflow)
+// now we grab that file and run Cypress tests against it
+// see https://github.com/netlify/cli/blob/master/src/commands/deploy.js
+const deploy = require('deploy.json')
+console.log('will run Cypress tests against deployed url', deploy.deploy_url)
+
+const cypress = require('cypress')
+cypress
+	.run({
+		config: {
+			baseUrl: deploy.deploy_url,
+		},
+	})
+	.then(results => {
+		if (results.failures) {
+			// really bad crash or cannot run tests
+			console.error(results.message)
+			process.exit(results.failures)
+		}
+		process.exit(results.totalFailed)
+	})
