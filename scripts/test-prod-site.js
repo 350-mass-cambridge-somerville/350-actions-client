c4dd07114820e8d015adaf5b644131d2cd19e48b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-10 21:21:24 -0400

    add prod deploy tests

---
+++ scripts/test-prod-site.js
@@ -0,0 +1,27 @@
+// after Netlify deploys the production site
+// we save deploy results in `deploy.json` file (see Netlify GH workflow)
+// now we grab that file and run some Cypress tests against it
+const path = require('path')
+const filename = path.join(process.cwd(), 'deploy.json')
+console.log('loading Netlify deploy results from file %s', filename)
+const deploy = require(filename)
+console.log('will run Cypress tests against deployed url', deploy.deploy_url)
+
+const cypress = require('cypress')
+cypress
+	.run({
+		// only run tests safe against data modification
+		// and with minimal network stubbing
+		spec: 'cypress/integration/prod-spec.js',
+		config: {
+			baseUrl: deploy.deploy_url,
+		},
+	})
+	.then(results => {
+		if (results.failures) {
+			// really bad crash or cannot run tests
+			console.error(results.message)
+			process.exit(results.failures)
+		}
+		process.exit(results.totalFailed)
+	})
