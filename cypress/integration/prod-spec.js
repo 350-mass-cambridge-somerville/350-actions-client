b79b8ee6f5386666db56248f5af550e9095df7b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-11 21:34:08 -0400

    add prod deploy workflow and production tests (#18)
    * add prod deploy tests
    
    * skip prod tests on localhost

---
+++ cypress/integration/prod-spec.js
@@ -0,0 +1,27 @@
+// minimal number of tests that are safe to run
+// against the production deploy
+/// <reference types="cypress" />
+import { isOn } from '@cypress/skip-test'
+
+const describeOrSkip = isOn('localhost') ? describe.skip : describe
+
+describeOrSkip('prod tests', () => {
+	it('has the current card', () => {
+		cy.visit('/')
+		cy.contains('Action Card').should('be.visible')
+		cy.contains('Track my actions!').should('be.visible')
+		cy.get('[data-cy=action-check-display]').should('have.length.gt', 3)
+	})
+
+	it('goes to past', () => {
+		cy.visit('/')
+		cy.contains('a[href="/past"]', 'Past').click()
+		cy.location('href').should('match', /\/past$/)
+		cy.get('[data-cy="close-open-card"]').should('have.length.gt', 2)
+	})
+
+	// TODO: enable after deploying the /past redirect
+	it.skip('has past cards', () => {
+		cy.visit('/past')
+	})
+})
