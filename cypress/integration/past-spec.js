4d05032a2f87beafc30f65feb67f38841976057e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-28 12:06:23 -0400

    add failing test for #13 - undefined geography

--- cypress/integration/past-spec.js
+++ cypress/integration/past-spec.js
@@ -7,6 +7,11 @@ describe('past actions', () => {
 		cy.get('[data-cy=action-display]').should('have.length', 15)
 	}
 
+	const getExpansionCardDataCy = cardId => {
+		expect(cardId).to.be.a('number')
+		return `[data-cy=expansion-panel-${cardId}]`
+	}
+
 	it('shows past actions', () => {
 		cy.server()
 		cy.route('/actioncards/', 'fixture:actioncards')
@@ -26,7 +31,7 @@ describe('past actions', () => {
 		noActionsAreVisible()
 
 		cy.log('looking at a particular action card')
-		cy.get('[data-cy=expansion-panel-2]')
+		cy.get(getExpansionCardDataCy(2))
 			.find('[data-cy=close-open-card]')
 			.click()
 
@@ -34,9 +39,31 @@ describe('past actions', () => {
 		cy.get('[data-cy=action-display]:visible').should('have.length', 4)
 
 		cy.log('closing card')
-		cy.get('[data-cy=expansion-panel-2]')
+		cy.get(getExpansionCardDataCy(2))
 			.find('[data-cy=close-open-card]')
 			.click()
 		noActionsAreVisible()
+	})
+
+	it('does not show UNDEFINED for geography', () => {
+		// fixture where we forgot to specify geography region
+		// and it got set to the default "UNDEFINED" string
+		const fixtureName = 'undefined-geography'
+		cy.fixture(fixtureName)
+			.its('0.actions.0.geography_type')
+			.should('equal', 'UNDEFINED')
+
+		cy.server()
+		cy.route('/actioncards/', 'fixture:' + fixtureName)
+		cy.visit('/past')
+		cy.get(getExpansionCardDataCy(5)) // card id from the the fixture
+			.click()
+
+		// card text is visible
+		cy.contains('Build the climate movement by').should('be.visible')
+
+		// even if the card has geography set to string "UNDEFINED"
+		// it should not show up in the UI
+		cy.contains('UNDEFINED').should('not.exist')
 	})
 })
